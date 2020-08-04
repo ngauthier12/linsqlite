43dae33f0d290f4d9c8765a418dfdb32f6ef7583
Nicolas Gauthier <ngauthier12@gmail.com>
2020-08-04 11:22:20 -0400

    added test_column metadata test.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -18,7 +18,13 @@ class TestMetaData(unittest.TestCase):
         os.remove(self.temp_db_path)
 
     def test_tables(self):
-        expected_table_names = ["cars"]
+        expected_table_names = ['cars']
         tables = self.connection.get_tables()
         table_names = list(map(lambda x: x.name, tables))
         self.assertEqual(expected_table_names, table_names)
+
+    def test_columns(self):
+        expected_column_names = ['make', 'model', 'is_electric', 'year', 'id']
+        columns = self.connection.cars.get_columns()
+        column_names = list(map(lambda x: x.name, columns))
+        self.assertEqual(expected_column_names, column_names)
